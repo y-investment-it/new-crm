498a63fa5626d644b9dc4407a2f6f49abb8e8827
7ossam26 <amedoss17@gmail.com>
2025-11-04 14:41:27 +0200

    first push

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,19 +3,16 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array<int, string>|string|null
-     */
     protected $proxies;
 
-    /**
-     * The headers that should be used to detect proxies.
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+        SymfonyRequest::HEADER_X_FORWARDED_FOR |
+        SymfonyRequest::HEADER_X_FORWARDED_HOST |
+        SymfonyRequest::HEADER_X_FORWARDED_PORT |
+        SymfonyRequest::HEADER_X_FORWARDED_PROTO |
+        SymfonyRequest::HEADER_X_FORWARDED_AWS_ELB; // keep if you might sit behind ELB
 }
